899e876d4484ac57b4008240eb2b37a93581d760
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2020-09-16 14:02:07 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
   next();
 });
 
