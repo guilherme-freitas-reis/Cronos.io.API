53ae605e3922dc9b6c14d62887727a50cbdbd82d
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2020-09-16 14:09:46 -0300

    Update newsletter.controller.js

--- src/controllers/newsletter.controller.js
+++ src/controllers/newsletter.controller.js
@@ -21,10 +21,6 @@ exports.index = async function (req, res) {
 };
 
 exports.create = async function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  
   let newsletter = new Newsletter({
     name: req.body.name,
     email: req.body.email,
