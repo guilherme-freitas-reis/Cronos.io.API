d650db5cdf4c708b20028baed48851373c67186f
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2020-09-16 14:14:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.options('*', cors()); 
 
 app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
     next();
